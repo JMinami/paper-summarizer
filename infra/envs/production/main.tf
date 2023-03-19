c2139a88f95b820441e205a6cbb2a4c709341663
Jminami <mj14rugby@yahoo.co.jp>
2023-03-19 09:58:42 +0900

    terraformの環境設定

---
+++ infra/envs/production/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.57.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "paper-summarizer-tfstate"
+    prefix = "terraform/production"
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file_path)
+  project = "paper-summarizer-381022"
+  region = "asia-northeast1"
+}
+
+locals {
+  services_to_enable = [
+    # "cloudresourcemanager.googleapis.com",
+    "translate.googleapis.com",
+  ]
+}
+resource "google_project_service" "translation_api" {
+  project = var.project_id
+  for_each = toset(local.services_to_enable)
+
+  service = each.value
+  disable_dependent_services = true
+  disable_on_destroy         = false
+}
