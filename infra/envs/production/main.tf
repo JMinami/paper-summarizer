1b85403583f8895a0eed33b8dc66089473919e0b
Jminami <mj14rugby@yahoo.co.jp>
2023-03-26 10:40:32 +0900

    auth

--- infra/envs/production/main.tf
+++ infra/envs/production/main.tf
@@ -37,12 +37,38 @@ resource "google_project_service" "translation_api" {
 }
 
 resource "google_service_account" "github-actions" {
-  project = var.project_id
-  account_id = "github-actions"
+  project      = var.project_id
+  account_id   = "github-actions"
   display_name = "A service account for GitHub Actions"
+}
+
+resource "google_service_account_iam_binding" "github-actions" {
+  service_account_id = google_service_account.github-actions.name
+  role = "roles/iam.workloadIdentityUser"
+  members = [
+    "principalSet://iam.googleapis.com/projects/571905713425/locations/global/workloadIdentityPools/github-actions/*"
+  ]
 }
 
 resource "google_project_service" "project" {
   project = var.project_id
   service = "iamcredentials.googleapis.com"
+}
+
+resource "google_iam_workload_identity_pool" "github-actions" {
+  workload_identity_pool_id = "github-actions"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github-actions-provider" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github-actions.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-actions-provider"
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.repository" = "assertion.repository"
+    "attribute.actor"      = "assertion.actor"
+    "attribute.aud"        = "assertion.aud"
+  }
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
 }
